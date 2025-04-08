a76eb26a94635f2c9ffbabbe39f19299e52141e6
DevMatheusMarques <estudosfatecmhcm@gmail.com>
2025-04-08 18:16:05 -0300

    feat: Finalizado componente de consultas.

---
+++ src/app/components/consultations/consultations.component.ts
@@ -0,0 +1,387 @@
+import {Component, Input, ViewChild} from '@angular/core';
+import {Column, DataTableComponent} from '../data-table/data-table.component';
+import {DynamicModalComponent} from '../dynamic-modal/dynamic-modal.component';
+import {MaskDirective} from '../../shared/directives/mask.directive';
+import {HttpClient, HttpClientModule, HttpHeaders} from '@angular/common/http';
+import {ToastService} from '../../shared/services/toast.service';
+import Swal from 'sweetalert2';
+import {forkJoin} from 'rxjs';
+import {VerifyDataService} from '../../shared/services/verifyData.service';
+
+interface Consultation {
+  id: number;
+  patient: {
+    id: number;
+    name: string;
+    telephone: string;
+  };
+  doctor: {
+    id: number;
+    name: string;
+  };
+  date: string;
+  time: string;
+  status: string;
+  observations: string;
+}
+
+interface Patient {
+  id: number;
+  name: string;
+  email: string;
+  telephone: string;
+  status: string;
+}
+
+interface Doctor {
+  id: number;
+  name: string;
+  email: string;
+  crm: string;
+  telephone: string;
+  status: string;
+}
+
+@Component({
+  selector: 'app-consultations',
+  imports: [
+    DataTableComponent,
+    HttpClientModule,
+    DynamicModalComponent,
+  ],
+  templateUrl: './consultations.component.html',
+  styleUrl: './consultations.component.css'
+})
+export class ConsultationsComponent {
+  @Input() selectedTab: string = '';
+  @ViewChild(MaskDirective) maskDirective!: MaskDirective;
+  showModal = false;
+  viewMode = false;
+  modalTitle = '';
+  modalFields: any[] = [];
+  editingConsultationId = '';
+
+  consultationsColumns: Column[] = [
+    { key: 'patient.name', header: 'Paciente', type: 'text' },
+    { key: 'doctor.name', header: 'Médico', type: 'text' },
+    { key: 'date', header: 'Data da Consulta', type: 'text' },
+    { key: 'time', header: 'Horário', type: 'text' },
+    { key: 'status', header: 'Status', type: 'status' },
+    { key: 'action', header: 'Ações', type: 'action' }
+  ];
+
+  consultationData: Consultation[] = [];
+
+  doctorData: Doctor[] = [];
+  patientData: Patient[] = [];
+
+  private apiUrl = 'http://localhost:8080/api/consultations';
+
+  constructor(private http: HttpClient, private toastService: ToastService, private verifyDataService: VerifyDataService) {}
+
+  ngOnInit(): void {
+    this.loadConsultations();
+  }
+
+  loadConsultations(): void {
+    const token = localStorage.getItem('authToken');
+
+    if (!token) {
+      console.error('Token não encontrado!');
+      return;
+    }
+
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+
+    this.http.get<Consultation[]>(this.apiUrl, { headers }).subscribe((data) => {
+      this.consultationData = data;
+    });
+  }
+
+  openModal() {
+    this.selectedTab = 'Consultas';
+    this.modalTitle = 'Adicionar Consulta';
+
+    const token = localStorage.getItem('authToken');
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+
+    // Chamar os dois endpoints ao mesmo tempo usando forkJoin
+    forkJoin({
+      doctors: this.http.get<Doctor[]>(`http://localhost:8080/api/doctors`, { headers }),
+      patients: this.http.get<Patient[]>(`http://localhost:8080/api/patients`, { headers })
+    }).subscribe(({ doctors, patients }) => {
+      this.doctorData = doctors;
+      this.patientData = patients;
+      this.modalFields = [
+        {
+          label: 'Paciente',
+          name: 'patient',
+          type: 'select',
+          value: '',
+          placeholder: 'Selecione o paciente',
+          options: this.patientData.map(patient => ({
+            label: patient.name,
+            value: patient.id
+          }))
+        },
+        {
+          label: 'Médico',
+          name: 'doctor',
+          type: 'select',
+          value: '',
+          placeholder: 'Selecione o médico',
+          options: this.doctorData.map(doctor => ({
+            label: doctor.name,
+            value: doctor.id
+          }))
+        },
+        { label: 'Data da Consulta', name: 'date', type: 'date', value: '' },
+        { label: 'Horário da Consulta', name: 'time', type: 'time', value: '' },
+      ];
+
+      this.viewMode = false;
+      this.showModal = true;
+    });
+  }
+
+  closeModal() {
+    this.showModal = false;
+    this.viewMode = false;
+  }
+
+  handleSubmit() {
+    if (!this.verifyDataService.verifyData(this.modalFields, this.selectedTab)) return;
+
+    const formData: any = {};
+    this.modalFields.forEach(field => {
+      formData[field.name] = field.value;
+    });
+
+    let formattedDate = '';
+    if (formData['date']) {
+      const dateObj = new Date(formData['date'] + 'T12:00:00');
+      formattedDate = dateObj.toLocaleDateString('pt-BR');
+    }
+
+    let dataToSend: any = {
+      patient: { id: formData['patient'] },
+      doctor: { id: formData['doctor'] },
+      date: formattedDate,
+      time: formData['time'],
+      status: 'Pendente',
+      observations: formData['observations'] || ''
+    };
+
+
+    const token = localStorage.getItem('authToken');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+    const endpoint = 'http://localhost:8080/api/consultations';
+    this.http.post(endpoint, dataToSend, { headers }).subscribe(
+      response => {
+        this.closeModal();
+        this.loadConsultations();
+        this.toastService.showToast('Consulta cadastrada com sucesso!', 'success');
+      },
+      error => {
+        const errorMessage = error.error?.message || 'Erro desconhecido';
+        this.toastService.showToast(errorMessage, 'error');
+      }
+    );
+  }
+
+  editConsultation(consultation: any): void {
+    this.selectedTab = 'Consultas';
+    this.modalTitle = 'Editar Consulta';
+
+    this.editingConsultationId = consultation.id;
+
+    const formattedDate = consultation.date ? formatDateString(consultation.date) : '';
+
+    function formatDateString(dateStr: string): string {
+      const parts = dateStr.split('/');
+      if (parts.length === 3) {
+        return new Date(`${parts[2]}-${parts[1]}-${parts[0]}`).toISOString().split('T')[0];
+      }
+      return new Date(dateStr).toISOString().split('T')[0];
+    }
+
+    const token = localStorage.getItem('authToken');
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+
+    forkJoin({
+      doctors: this.http.get<Doctor[]>(`http://localhost:8080/api/doctors`, { headers }),
+      patients: this.http.get<Patient[]>(`http://localhost:8080/api/patients`, { headers })
+    }).subscribe(({ doctors, patients }) => {
+      this.doctorData = doctors;
+      this.patientData = patients;
+      this.modalFields = [
+        {
+          label: 'Paciente',
+          name: 'patient',
+          type: 'select',
+          value: consultation.patient.id,
+          placeholder: 'Selecione o paciente',
+          options: this.patientData.map(patient => ({
+            label: patient.name,
+            value: patient.id
+          }))
+        },
+        {
+          label: 'Médico',
+          name: 'doctor',
+          type: 'select',
+          value: consultation.doctor.id,
+          placeholder: 'Selecione o médico',
+          options: this.doctorData.map(doctor => ({
+            label: doctor.name,
+            value: doctor.id
+          }))
+        },
+        { label: 'Data da Consulta', name: 'date', type: 'date', value: formattedDate },
+        { label: 'Horário da Consulta', name: 'time', type: 'time', value: consultation.time },
+        {
+          label: 'Status',
+          name: 'status',
+          type: 'radio',
+          value: consultation.status,
+          options: [
+            { label: 'Pendente', value: 'Pendente' },
+            { label: 'Finalizada', value: 'Finalizada' },
+            { label: 'Cancelada', value: 'Cancelada' }
+          ]
+        },
+      ];
+
+      this.viewMode = false;
+      this.showModal = true;
+    });
+  }
+
+
+  handleSubmitEdit() {
+    if (!this.verifyDataService.verifyData(this.modalFields, this.selectedTab)) return;
+
+    const formData: any = {};
+    this.modalFields.forEach(field => {
+      formData[field.name] = field.value;
+    });
+
+    let formattedDate = '';
+    if (formData['birth_date']) {
+      const dateObj = new Date(formData['birth_date'] + 'T12:00:00');
+      formattedDate = dateObj.toLocaleDateString('pt-BR');
+    }
+
+    let dataToSend: any = {
+      patient: { id: formData['patient'] },
+      doctor: { id: formData['doctor'] },
+      date: formattedDate,
+      time: formData['time'],
+      status: formData['status'],
+      observations: formData['observations'] || ''
+    };
+
+    console.log(this.editingConsultationId)
+
+    const token = localStorage.getItem('authToken');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+    const endpoint = `http://localhost:8080/api/consultations/${this.editingConsultationId}`;
+    this.http.put(endpoint, dataToSend, { headers }).subscribe(
+      response => {
+        this.closeModal();
+        this.loadConsultations();
+        this.toastService.showToast('Consulta atualizada com sucesso!', 'success');
+      },
+      error => {
+        const errorMessage = error.error?.message || 'Erro desconhecido';
+        this.toastService.showToast(errorMessage, 'error');
+      }
+    );
+  }
+
+  deleteConsultation(consultation: any): void {
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: "bg-green-600 hover:bg-green-700 text-white py-2 px-4 rounded",
+        cancelButton: "bg-red-600 hover:bg-red-700 text-white py-2 px-4 rounded",
+        actions: "flex justify-center space-x-4"
+      },
+      buttonsStyling: false
+    });
+    swalWithBootstrapButtons.fire({
+      title: "Tem certeza que deseja excluir esta consulta?",
+      text: "Essa ação não poderá ser desfeita!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Sim, excluir!",
+      cancelButtonText: "Não, cancelar!",
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const token = localStorage.getItem('authToken');
+        const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+        const endpoint = `http://localhost:8080/api/consultations/${consultation.id}`;
+        this.http.delete(endpoint, { headers }).subscribe(
+          response => {
+            this.loadConsultations();
+            this.toastService.showToast('Consulta excluída com sucesso!', 'success');
+          },
+          error => {
+            const errorMessage = error.error?.message || 'Erro desconhecido';
+            this.toastService.showToast(errorMessage, 'error');
+          }
+        );
+      }
+    });
+  }
+
+  viewConsultation(consultation: any): void {
+    this.selectedTab = 'Consultas';
+    this.modalTitle = 'Dados da Consulta';
+
+    this.modalFields = [
+      { label: 'Paciente', name: 'patient', type: 'text', value: consultation.patientName, placeholder: 'Nome do paciente', readOnly: true },
+      { label: 'Médico', name: 'doctor', type: 'text', value: consultation.doctorName, placeholder: 'Nome do médico', readOnly: true },
+      { label: 'Data da Consulta', name: 'date', type: 'date', value: consultation.date, readOnly: true },
+      { label: 'Horário da Consulta', name: 'time', type: 'time', value: consultation.time, readOnly: true },
+    ];
+    this.viewMode = true;
+    this.showModal = true;
+  }
+
+  private removePhoneMask(value: string): string {
+    if (!value) return '';
+    return value.replace(/\D/g, '');
+  }
+
+  sendMessage(item: any) {
+    let phoneNumber = item.patient.telephone;
+
+    const patientName = item.patient.name;
+
+    const consultationDate = item.date;
+    const consultationTime = item.time;
+
+    // 1. Remove todos os caracteres não numéricos
+    phoneNumber = phoneNumber.replace(/\D/g, '');
+
+    if (!phoneNumber.startsWith('55')) {
+      phoneNumber = '55' + phoneNumber;
+    }
+
+    const message = `Olá, ${patientName}! Estamos entrando em contato para falar sobre sua consulta agendada para o dia ${consultationDate} as ${consultationTime}.`;
+    const encodedMessage = encodeURIComponent(message);
+    const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
+
+    window.open(whatsappUrl, '_blank');
+  }
+}
