de21514fef2d92c2e331aaf750e61909044bf88c
DevMatheusMarques <estudosfatecmhcm@gmail.com>
2025-04-08 18:10:55 -0300

    refactor: Ajuste de arquivos principais.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import {Router, RouterModule, RouterOutlet} from '@angular/router';
 import { CommonModule } from '@angular/common';
 import {SidebarComponent} from './components/sidebar/sidebar.component';
 import {ToastContainerComponent} from './components/toast-container/toast-container.component';
+import {SweetAlert2Module} from '@sweetalert2/ngx-sweetalert2';
+import {NgChartsModule} from 'ng2-charts';
 
 
 @Component({
@@ -14,6 +16,8 @@ import {ToastContainerComponent} from './components/toast-container/toast-contai
     RouterModule,
     SidebarComponent,
     ToastContainerComponent,
+    SweetAlert2Module,
+    NgChartsModule
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,17 @@
 import { Routes } from '@angular/router';
 import {AuthComponent} from './components/auth/auth.component';
 import {DashboardComponent} from './components/dashboard/dashboard.component';
-import {PatientsComponent} from './components/patients/list/patients.component';
-import {DoctorsComponent} from './components/doctors/list/doctors.component';
+import {PatientsComponent} from './components/patients/patients.component';
+import {DoctorsComponent} from './components/doctors/doctors.component';
+import {ConsultationsComponent} from './components/consultations/consultations.component';
+import {UsersComponent} from './components/users/users.component';
 
 export const routes: Routes = [
   { path: '', component: AuthComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'patients', component: PatientsComponent },
   { path: 'doctors', component: DoctorsComponent },
+  { path: 'consultations', component: ConsultationsComponent },
+  { path: 'users', component: UsersComponent },
   { path: '**', redirectTo: '' }
 ];
